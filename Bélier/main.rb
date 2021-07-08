02626db705fb6e5144f76251bf75f833c8ddb802
jarod25 <63459117+jarod25@users.noreply.github.com>
2021-07-08 12:50:19 +0200

    Le bon ruby !

--- Bélier/main.rb
+++ Bélier/main.rb
@@ -1,7 +1,9 @@
+require "./verification"
+
 def getRubies()
   ruby=""
   allrubies = []
-  File.read("C:\Users\PC\Downloads\Feu_-_Participants.zip\Feu - Participants\Bélier - Participant").readlines("rubies.txt").each do |line|
+  File.readlines("rubies.txt").each do |line|
       if line.length != 1
           ruby = ruby + line
       else
