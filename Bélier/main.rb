ec9671d0a9b00446be3440d90b95bc81952075b0
jarod25 <63459117+jarod25@users.noreply.github.com>
2021-07-08 12:28:02 +0200

    Update main.rb

--- Bélier/main.rb
+++ Bélier/main.rb
@@ -1,1 +1,18 @@
-getrubies()
+def getRubies()
+  ruby=""
+  allrubies = []
+  File.read("C:\Users\PC\Downloads\Feu_-_Participants.zip\Feu - Participants\Bélier - Participant").readlines("rubies.txt").each do |line|
+      if line.length != 1
+          ruby = ruby + line
+      else
+        allrubies<< ruby
+        ruby = ""
+      end
+  end
+  return allrubies
+end
+
+allRubies = getRubies()
+allRubies.each do |ruby|
+verification(ruby)
+end
